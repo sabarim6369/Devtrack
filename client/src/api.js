4ee45b85cbc1f0307dc0d29436cbde26cf054ed9
sabarim6369 <sabarim6369@gmail.com>
2026-02-14 17:51:30 +0530

    api work done1

--- client/src/api.js
+++ client/src/api.js
@@ -1,2 +1,3 @@
 // Central API configuration
-export const API_URL = 'http://localhost:5000';
+// export const API_URL = 'http://localhost:5000';
+export const API_URL = 'https://devtrack-ybd5.onrender.com';
